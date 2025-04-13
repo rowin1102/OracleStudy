3504f6f3d7a6cfdb551e12ffc359487e5e6c3b8c
rowin1102 <rowin1102@naver.com>
2025-04-13 21:35:32 +0900

    Or11Constraint

--- Or11Constraint.sql
+++ Or11Constraint.sql
@@ -417,3 +417,55 @@ insert into tb_check2 values (9);
 insert into tb_check2 values (10);
 -- 11은 제약조건 위배로 입력 실패. 오류 발생됨.
 insert into tb_check2 values (11);
+
+--------------------------------------------------------------------------------
+
+/*
+1. emp 테이블의 구조를 복사하여 pr_emp_const 테이블을 만드시오. 
+복사된 테이블의 사원번호 칼럼에 pr_emp_pk 라는 이름으로 primary key 
+제약조건을 지정하시오.
+*/
+create table pr_emp_const
+as
+SELECT * FROM emp
+where 1=0;
+
+alter table pr_emp_const add constraint pr_emp_pk
+    primary key (empno);
+
+/*
+2. dept 테이블의 구조를 복사해서 pr_dept_const 테이블을 만드시오. 
+부서번호에 pr_dept_pk 라는 제약조건명으로 primary_key를 생성하시오.
+*/
+create table pr_dept_const
+as
+SELECT * FROM dept
+where 1=0;
+
+alter table pr_dept_const add constraint pr_dept_pk
+    primary key (deptno);
+
+/*
+3. pr_dept_const 테이블에 존재하지 않는 부서의 사원이 배정되지 않도록 외래키 
+제약조건을 지정하되 제약조건 이름은 pr_emp_dept_fk 로 지정하시오.
+*/
+alter table pr_emp_const add constraint pr_emp_dept_fk
+    foreign key (deptno) references pr_dept_const (deptno);
+
+/*
+4. pr_emp_const 테이블의 comm 칼럼에 0보다 큰 값만을 입력할수 있도록 
+제약조건을 지정하시오. 제약조건명은 지정하지 않아도 된다.
+*/
+
+alter table pr_emp_const add check (comm > 0);
+
+/*
+5. 위 3번에서는 두 테이블간에 외래키가 설정되어서 pr_dept_const 테이블에서 
+레코드를 삭제할 수 없었다. 이 경우 부모 레코드를 삭제할 경우 자식까지 같이 
+삭제될수 있도록 외래키를 지정하시오.
+*/
+alter table pr_emp_const drop constraint pr_emp_dept_fk;
+
+alter table pr_emp_const add constraint pr_emp_dept_fk
+    foreign key (deptno) references pr_dept_const (deptno)
+        on delete cascade;
